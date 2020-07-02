71837f9a10ed5d2a9ee35579ace49a8f14554ee1
Jameson Midgley <jameson.midgley@gmail.com>
2020-07-01 23:14:20 -0600

    started script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 // api key: 46bc2c42a8260a4e45e15ae9d7661e9b
 // api url: api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+var input = $("input");
+var currentDate = moment().format("MM/DD/YYYY");
+console.log(currentDate);
 
+function WeatherDashboard () {
 
+    $(document).ready(function () {
+
+        $("#submitBtn").on("click", function(event) {
+            event.preventDefault();
+            console.log("submit button correctly submit");
+            var searchCity = $(this).siblings("input").val().trim();
+            console.log(searchCity);
+            selectedCity(searchCity);
+        });
+ 
+    });
+};
+
+function selectedCity(searchCity) {
+    var APIKey = "46bc2c42a8260a4e45e15ae9d7661e9b"
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + APIKey;
+    console.log(queryURL);
+
+}
+
+WeatherDashboard();
