344abb8c1075a28a8e716efb615f83834a19341e
Jameson Midgley <jameson.midgley@gmail.com>
2020-07-02 19:12:33 -0600

    current temps displayed

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function WeatherDashboard () {
         $("#submitBtn").on("click", function(event) {
             event.preventDefault();
             console.log("submit button correctly submit");
-            var searchCity = $(this).siblings("input").val().trim();
+            var searchCity = $(this).siblings("input").val().toString().trim();
             console.log(searchCity);
             selectedCity(searchCity);
         });
@@ -21,8 +21,45 @@ function WeatherDashboard () {
 
 function selectedCity(searchCity) {
     var APIKey = "46bc2c42a8260a4e45e15ae9d7661e9b"
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" +"&q=" + searchCity + "&appid=" + APIKey;
     console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        console.log(response);
+
+        var currentCity = response.city.name;
+        console.log(currentCity);
+
+        var currentCondition = response.list[0].weather[0].icon;
+        console.log(currentCondition);
+        var iconImg = "http://openweathermap.org/img/w/" + currentCondition + ".png";
+
+        var currentTemp = response.list[0].main.temp;
+        var tempF = ((currentTemp - 273.15) * 1.80 + 32);
+        console.log(tempF);
+
+        var currentHumidity = response.list[0].main.humidity;
+        console.log(currentHumidity);
+
+        var currentWind = response.list[0].wind.speed;
+        console.log(currentWind);
+
+        $("#currentCity").text(currentCity).append(" " + currentDate).append($("#currentCondition").attr("src", iconImg));
+        $("#currentTemp").text("Temperature: " + tempF.toFixed(1) + "°F");
+        $("#currentHumidity").text("Humidity: " + currentHumidity + "%");
+        $("#currentWind").text("Wind Speed: " + currentWind);
+
+        
+        
+
+
+
+        // currentUV = 
+
+    })
 
 }
 
