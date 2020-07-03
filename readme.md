883ca4d8dc5ccf919cdd72a063188c9b7c1db669
Jameson Midgley <jameson.midgley@gmail.com>
2020-07-02 23:53:49 -0600

    final commit

---
+++ readme.md
@@ -0,0 +1,44 @@
+# weather-dashboard-hw6
+-------------
+## Introduction
+-------------
+Within this assignment, I am tasked with building a weather dashboard for
+travelers to see upcoming weather on trips. I am mostly tasked here with using third-party API's to gather weather information and display it on the screen.
+
+## Criteria
+-------------
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+WHEN I open the weather dashboard
+THEN I am presented with the last searched city forecast
+```
+
+## Summary
+-------------
+I feel like I am understanding concepts better! jQuery and AJAX are very useful tools in the way we can format our JavaScript. This was fun to get API calls to get current weather data. It makes the page so much more interactive so users only need to input a city at any moment to get what is going on there. Had a blast working through this assignment. Looking forward for more learning!
+
+## Links and Screenshots
+-------------
+
+[Link to my deployed Weather Dashboard](https://j-midgley13.github.io/weather-dashboard-hw6/)
+
+<img src="assets/page-beginning.png" alt="beginning" />
+
+<br>
+
+<img src="assets/searchbar-input.png" alt="search bar" />
+
+<br>
+
+<img src="assets/suggested-search.png" alt="suggested search" />
--- script.js
+++ script.js
@@ -11,8 +11,17 @@ function WeatherDashboard() {
         $("#submitBtn").on("click", function (event) {
             event.preventDefault();
             console.log("submit button correctly submit");
-            var searchCity = $(this).siblings("input").val().toString().trim();
-            
+            var searchCity = $(this).siblings("input").val().trim();
+            localStorage.setItem("City Search", JSON.stringify(searchCity));
+            console.log(searchCity);
+            selectedCity(searchCity);
+        });
+
+        $("#cityIdea button").on("click", function (event) {
+            event.preventDefault();
+
+            var searchCity = $(this).html().toString();
+            localStorage.setItem("City Search", JSON.stringify(searchCity));
             console.log(searchCity);
             selectedCity(searchCity);
         });
@@ -20,7 +29,9 @@ function WeatherDashboard() {
     });
 };
 
+// Starts function to fill in information for selected city
 function selectedCity(searchCity) {
+
     var APIKey = "46bc2c42a8260a4e45e15ae9d7661e9b"
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + "&q=" + searchCity + "&appid=" + APIKey;
     console.log(queryURL);
@@ -30,6 +41,9 @@ function selectedCity(searchCity) {
     })
         .then(function (response) {
             console.log(response);
+
+            var cityLon = response.city.coord.lon;
+            var cityLat = response.city.coord.lat;
 
             //logs current day weather conditions
             var currentCity = response.city.name;
@@ -54,19 +68,6 @@ function selectedCity(searchCity) {
             $("#currentHumidity").text("Humidity: " + currentHumidity + "%");
             $("#currentWind").text("Wind Speed: " + currentWind);
 
-            // var date2 = moment().add(1, 'd').format("MM/DD/YYYY")
-            // console.log(date2);
-            // var conditions2 = response.list[1].weather[0].icon;
-            // console.log(conditions2);
-            // var icon2 = "http://openweathermap.org/img/w/" + conditions2 + ".png";
-            // var temp2 = (((response.list[1].main.temp) - 273.15) * 1.80 + 32).toFixed(1);
-            // var humidity2 = response.list[1].main.humidity;
-
-            // $("#nextDay").text(date2);
-            // $("#nextDayC").attr("src", icon2);
-            // $("#nextDayT").text("Temp: " + temp2);
-            // $("#nextDayH").text("Humidity: " + humidity2);
-
             //Yakini from my group gave me great inspiration and code for this for loop!
             $("#forecast").empty();
 
@@ -81,14 +82,41 @@ function selectedCity(searchCity) {
                         `<p>Temp: ${((response.list[i].main.temp - 273.15) * 1.80 + 32).toFixed(1)}&nbsp;°F</p>` +
                         `<p>Humidity: ${response.list[i].main.humidity}%</p>` +
                         '</div>'
-                        );
+                    );
                 };
 
                 $("#forecast").append(forecast5day(i));
             };
 
+            $("#currentUV").empty();
+
+            var query2URL = "http://api.openweathermap.org/data/2.5/uvi?&appid=" + APIKey + "&lat=" + cityLat + "&lon=" + cityLon;
+
+            // Gets UV index and background color for severity
+            $.ajax({
+                url: query2URL,
+                method: "GET"
+            })
+                .then(function (response) {
+                    console.log(response);
+                    var currentUV = response.value;
+                    console.log(currentUV);
+                    if (currentUV < 3) {
+                        var backgroundUV = $("<span>").attr("style", "background-color: green").text(currentUV);
+                    } else if (currentUV >= 3 && currentUV < 5) {
+                        var backgroundUV = $("<span>").attr("style", "background-color: yellow").text(currentUV);
+                    } else if (currentUV >= 5 && currentUV < 8) {
+                        var backgroundUV = $("<span>").attr("style", "background-color: orange").text(currentUV);
+                    } else if (currentUV >= 8) {
+                        var backgroundUV = $("<span>").attr("style", "background-color: red").text(currentUV);
+                    }
+                    $("#currentUV").text("UV Index: ").append(backgroundUV);
+                });
+
 
         });
+
+
 
 }
 
